d1e97b2aa65a1ebcebdf501a6c18544430168f73
lokman-hekim <lokmanhekim@gmail.com>
2021-01-28 19:45:11 +0300

    New year chaos solved

--- NewYearChaos/Program.cs
+++ NewYearChaos/Program.cs
@@ -6,24 +6,66 @@ using System.Threading.Tasks;
 
 namespace NewYearChaos
 {
-    class Program
+    public class Program
     {
-        static void minimumBribes(int[] q)
+        public static string minimumBribes(int[] q)
         {
+            string retval = string.Empty;
 
+            int bribeCount = 0;
+            int diff = 0;
+            int[] idealOrder = new int[q.Length];
+            for (int i = 0; i < q.Length; i++)
+            {
+                idealOrder[i] = i + 1;
+            }
+
+            for (int i = 0; i < q.Length; i++)
+            {
+                if (idealOrder[i] != q[i])
+                {
+                    if (idealOrder[i + 1] == q[i])
+                    {
+                        diff = 1;
+                    }
+                    else if (idealOrder[i + 2] == q[i])
+                    {
+                        diff = 2;
+                    }
+                    else
+                    {
+                        retval = "Too chaotic";
+                        break;
+                    }
+   
+                    bribeCount += diff;
+                    //arrange ideal order
+                    for (int j = i + diff; j > i; j--)
+                    {
+                        idealOrder[j] = idealOrder[j - 1];
+                    }
+                    idealOrder[i] = q[i];
+
+                  
+                }
+                diff = 0;
+
+            }
+            if (retval == string.Empty)
+            {
+                retval = bribeCount.ToString();
+            }
+            Console.WriteLine(retval);
+            return retval;
 
         }
         static void Main(string[] args)
         {
-            int t = Convert.ToInt32(Console.ReadLine());
 
-            for (int tItr = 0; tItr < t; tItr++)
-            {
-                int n = Convert.ToInt32(Console.ReadLine());
+            int[] q = Array.ConvertAll("1,2,5,3,7,8,6,4 ".Split(','), qTemp => Convert.ToInt32(qTemp));
+            Console.WriteLine(minimumBribes(q));
+            Console.ReadLine();
 
-                int[] q = Array.ConvertAll(Console.ReadLine().Split(' '), qTemp => Convert.ToInt32(qTemp));
-                minimumBribes(q);
-            }
         }
     }
 }
