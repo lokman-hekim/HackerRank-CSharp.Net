4ef15bc7ac5996f354c6f16fd14675c9a3bdaf3a
lokman-hekim <lokmanhekim@gmail.com>
2021-01-31 16:55:53 +0300

    Hash Tables Ransom Note

---
+++ Hash Tables Ransom Note/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hash_Tables_Ransom_Note
+{
+ public   class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            string[] magazine = "two times three is not four".Split(' ');
+
+            string[] note = "two times two is four".Split(' ');
+
+            checkMagazine(magazine, note);
+            Console.ReadLine();
+        }
+
+        public static string checkMagazine(string[] magazine, string[] note)
+        {
+            Dictionary<string, int> magWords = new Dictionary<string, int>();
+            foreach (string item in magazine)
+            {
+                if (magWords.Keys.Contains(item))
+                {
+                    magWords[item]++;
+                }
+                else
+                {
+                    magWords.Add(item, 1);
+                }
+            }
+
+            string msg = "Yes";
+            for (int i = 0; i < note.Length; i++)
+            {
+                if (magWords.Keys.Contains(note[i]))
+                {
+                    if (magWords[note[i]] > 0)
+                    {
+                        magWords[note[i]]--;
+                    }
+                    else
+                    {
+                        msg = "No";
+                        break;
+                    }
+
+                }
+                else
+                {
+                    msg = "No";
+                    break;
+                }
+            }
+            Console.WriteLine(msg);
+            return msg;
+        }
+    }
+}
---
+++ TestProject/UTHashTablesRansomNote.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Hash_Tables_Ransom_Note;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UTHashTablesRansomNote
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string magazine = "give me one grand today night";
+            string note = "give one grand today";
+            string result = Program.checkMagazine(magazine.Split(' '), note.Split(' '));
+            string expected = "Yes";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string magazine = "two times three is not four";
+            string note = "two times two is four";
+            string result = Program.checkMagazine(magazine.Split(' '), note.Split(' '));
+            string expected = "No";
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string magazine = "ive got a lovely bunch of coconuts";
+            string note = "ive got some coconuts";
+            string result = Program.checkMagazine(magazine.Split(' '), note.Split(' '));
+            string expected = "No";
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
