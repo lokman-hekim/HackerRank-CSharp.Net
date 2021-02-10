1b05785561abb7c093b1bb552c2bee0cb7d2dab9
lokman-hekim <lokmanhekim@gmail.com>
2021-02-10 20:56:54 +0300

    GameOfThrones

---
+++ GameOfThrones/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfThrones
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "cdcdcdcdeeeef";
+
+            string result = gameOfThrones(s);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+
+        public static string gameOfThrones(string s)
+        {
+            string retVal = "YES";
+
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            for (int i = 0; i < s.Length; i++)
+                if (counts.ContainsKey(s[i]))
+                    counts[s[i]]++;
+                else
+                    counts.Add(s[i], 1);
+
+            bool isOdd = false;
+            foreach (KeyValuePair<char, int> item in counts)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    if (isOdd == false)
+                    {
+                        isOdd = true;
+                    }
+                    else
+                    {
+                        retVal = "NO";
+                        break;
+                    }
+                }
+            }
+
+
+            return retVal;
+
+        }
+    }
+}
